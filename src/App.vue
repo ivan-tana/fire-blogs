bfa3669f787928e0e1146b586d514010b9fd506d
ivan-tana <84873112+ivan-tana@users.noreply.github.com>
2023-04-17 10:19:18 +0100

    project-setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:ital,wght@0,100;0,200;0,400;0,500;1,100;1,400&display=swap');
+
+#app {
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
